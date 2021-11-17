0eb02a27a52d944f7a7c1b4bf0d90aa86130ad30
Louis Gamor <https://github.com/lkgamor/>
2021-11-17 13:20:34 +0000

    Initial TDD Commit

---
+++ src/main/java/com/turntabl/tdd/Fibonacci.java
@@ -0,0 +1,21 @@
+package com.turntabl.tdd;
+/*
+TASK:
+Use TDD to write a function that calculates the first N number
+of the Fibonacci series for a given natural number N.
+
+TIPS:
+1. Work in pairs
+2. Be extreme
+   i. use baby steps
+   ii. define test, code, test
+3. commit often
+  i. show that you're really using TDD
+
+DEFINITION OF TRHE FIBONACCI SERIES
+The Fibonacci sequence is a series of numbers: 0 1 1 2 3 5 8 13 21 34 ...
+The next number is found by adding the two numbers before it.
+ */
+
+public class Fibonacci {
+}
---
+++ src/main/java/com/turntabl/tdd/MainApplication.java
@@ -0,0 +1,7 @@
+package com.turntabl.tdd;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("We're in business");
+    }
+}
